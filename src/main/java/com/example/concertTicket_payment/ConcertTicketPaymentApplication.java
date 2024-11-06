0d825d8faa4a4c243a12466bd207a82b5b936654
LeeJongbokZ <s2feeling@naver.com>
2024-11-06 23:34:17 +0900

    first commit

---
+++ src/main/java/com/example/concertTicket_payment/ConcertTicketPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.concertTicket_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertTicketPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertTicketPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/concertTicket_payment/ConcertTicketPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concertTicket_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertTicketPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
